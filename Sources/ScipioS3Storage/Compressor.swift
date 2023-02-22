e2b859cac7be0e91b0cc27f08898b95a5f54723c
giginet <giginet.net@gmail.com>
2023-02-22 14:05:03 +0900

    Fix compressor

--- Sources/ScipioS3Storage/Compressor.swift
+++ Sources/ScipioS3Storage/Compressor.swift
@@ -8,20 +8,27 @@ struct Compressor {
     private let fileManager: FileManager = .default
 
     func compress(_ directoryPath: URL) throws -> ByteStream {
-        let encodingStream = try createEncodingStream()
-
         guard let keySet else { throw Error.initializationError }
 
-        let xcframeworkPath = FilePath(directoryPath.path)
-        print(xcframeworkPath)
-        try encodingStream.writeDirectoryContents(
-            archiveFrom: xcframeworkPath,
-            keySet: keySet
-        )
+        let source = FilePath(directoryPath.path)
+
+        try ArchiveByteStream.withFileStream(
+            path: FilePath(archivePath.path),
+            mode: .writeOnly,
+            options: [.create, .truncate],
+            permissions: [.ownerReadWrite, .groupRead, .otherRead]
+        ) { file in
+            try ArchiveByteStream.withCompressionStream(using: .lzfse, writingTo: file) { compressor in
+                try ArchiveStream.withEncodeStream(writingTo: compressor) { encoder in
+                    try encoder.writeDirectoryContents(archiveFrom: source, keySet: keySet)
+                }
+            }
+        }
 
         guard let data = fileManager.contents(atPath: archivePath.path) else {
             throw Error.compressionError
         }
+        defer { try? fileManager.removeItem(at: archivePath) }
 
         return ByteStream.from(data: data)
     }
@@ -31,34 +38,8 @@ struct Compressor {
         case compressionError
     }
 
-    private func createEncodingStream() throws -> ArchiveStream {
-        let archiveFilePath = FilePath(archivePath.path)
-
-        guard let writeFileStream = ArchiveByteStream.fileStream(
-                path: archiveFilePath,
-                mode: .writeOnly,
-                options: [ .create ],
-                permissions: FilePermissions(rawValue: 0o644)) else {
-            throw Error.initializationError
-        }
-        defer { try? writeFileStream.close() }
-
-        guard let compressStream = ArchiveByteStream.compressionStream(
-                using: .lzfse,
-                writingTo: writeFileStream) else {
-            throw Error.initializationError
-        }
-        defer { try? compressStream.close() }
-
-        guard let encodeStream = ArchiveStream.encodeStream(writingTo: compressStream) else {
-            throw Error.initializationError
-        }
-        defer { try? encodeStream.close() }
-        return encodeStream
-    }
-
     private var archivePath: URL {
-        URL(fileURLWithPath: "/Users/jp30698/work/test").appendingPathComponent("xcframework.aar")
+        temporaryDirectory.appendingPathComponent("xcframework.aar")
     }
 
     private var temporaryDirectory: URL {
