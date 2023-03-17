cae017cefdbe700a95fdbf2cc51f062a84988661
giginet <giginet.net@gmail.com>
2023-03-17 17:26:44 +0900

    Rename package

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "ScipioS3Storage",
+    name: "scipio-s3-storage",
     platforms: [
         .macOS(.v12),
     ],
@@ -16,7 +16,6 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/giginet/Scipio.git",
                  branch: "support-async-cache"),
-        // .package(path: "/Users/jp30698/work/Scipio"),
         .package(url: "https://github.com/soto-project/soto-codegenerator", 
                  from: "0.6.0"),
         .package(url: "https://github.com/soto-project/soto-core.git", 
